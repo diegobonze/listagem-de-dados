3e110a040615d8a3774158e82ac89e57db595bf0
Diego Bonze <diegobonze747@gmail.com>
2024-03-05 21:15:14 -0300

    feat: base ui, list, filters & pagination

---
+++ src/app.tsx
@@ -0,0 +1,148 @@
+import { LucidePlus, LucideSearch, LucideFileDown, LucideMoreHorizontal, LucideFilter } from 'lucide-react'
+import { Header } from './components/header'
+import { Tabs } from './components/tabs'
+import { Button } from './components/ui/button'
+import { Control, Input } from './components/ui/input'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
+import { Pagination } from './components/pagination'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import { useSearchParams } from 'react-router-dom'
+import { useState } from 'react'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  amountOfVideos: number
+  id: string
+}
+
+
+export function App() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const urlFilter = searchParams.get('filter') ?? ''
+  
+  const [filter, setFilter] = useState(urlFilter)
+
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags', urlFilter, page],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`)
+      const data = await response.json()
+
+      // delay 2s
+      await new Promise(resolve => setTimeout(resolve, 2000))
+
+      console.log(data)
+
+      return data
+    },
+
+    placeholderData: keepPreviousData
+  })
+
+  if (isLoading) {
+    return null
+  }
+
+  function handleFilter() {
+    setSearchParams(params => {
+      params.set('page', "1")
+      params.set('filter', filter)
+
+      return params
+    })
+  }
+
+  return (
+    <div className="py-10 space-y-8">
+      <div>
+        <Header />
+        <Tabs />
+      </div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold">Tags</h1>
+          <Button variant='primary'>
+            <LucidePlus className="size-3" />
+            Create new
+          </Button>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className='flex items-center'>
+            <Input variant='filter'>
+              <LucideSearch className="size-3" />
+              <Control
+                placeholder="Search tags..."
+                onChange={e => setFilter(e.target.value)}
+                value={filter}
+              />
+            </Input>
+            <Button onClick={handleFilter}>
+              <LucideFilter className="size-3" />
+              Filter
+            </Button>
+          </div>
+
+          <Button>
+            <LucideFileDown className="size-3" />
+            Export
+          </Button>
+        </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tagsResponse?.data.map((tags) => {
+              return (
+                <TableRow key={tags.id}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="flex flex-col gap-0.5">
+                      <span className="font-medium">{tags.title}</span>
+                      <span className="text-xs text-zinc-500">{tags.id}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-zinc-300">
+                    {tags.amountOfVideos}
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Button size="icon">
+                      <LucideMoreHorizontal className="size-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+
+        {tagsResponse &&
+          <Pagination
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+            page={page}
+          />
+        }
+      </main>
+    </div>
+  )
+}
